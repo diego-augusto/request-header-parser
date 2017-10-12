064a2dec2f291dafbb67275b1d54b0c6f4ce273e
Diego Augusto <daugustomoura@gmail.com>
2017-10-12 02:50:25 -0300

    finaliza implementação

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+    res.json(
+        {
+            "ipaddress": req.ip,
+            "language": req.get("accept-language").split(',')[0],
+            "software": req.get("user-agent").split('(')[1].split(')')[0]
+        }
+    );
+
 });
 
 app.listen(3000);
