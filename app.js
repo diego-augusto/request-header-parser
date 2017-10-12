d92a94165a17008618530846e834b33b23d2fea4
Diego Augusto <daugustomoura@gmail.com>
2017-10-12 02:57:58 -0300

    tenta recuperar ip

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.get('/', function (req, res) {
 
     res.json(
         {
-            "ipaddress": req.connection.remoteAddress || request.get("x-forwarded-for"),
+            "ipaddress": req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress,
             "language": req.get("accept-language").split(',')[0],
             "software": req.get("user-agent").split('(')[1].split(')')[0]
         }
