99184f035cf29d055a9c57351b6511d58e350b3c
Diego Augusto <daugustomoura@gmail.com>
2017-10-12 03:01:01 -0300

    corrige ip e url

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/api/whoami/', function (req, res) {
+
+    const ip  = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress;
 
     res.json(
         {
-            "ipaddress": req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress,
+            "ipaddress": ip.split(',')[0],
             "language": req.get("accept-language").split(',')[0],
             "software": req.get("user-agent").split('(')[1].split(')')[0]
         }
